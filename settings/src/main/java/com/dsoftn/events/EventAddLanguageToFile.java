d36f372f6ab07541a9a533acea5d2e212c7a551f
DsoftN <dsoftn@gmail.com>
2024-11-30 18:49:50 +0100

    Added event for language manager

---
+++ settings/src/main/java/com/dsoftn/events/EventAddLanguageToFile.java
@@ -0,0 +1,38 @@
+package com.dsoftn.events;
+
+import javafx.event.Event;
+import javafx.event.EventType;
+
+import com.dsoftn.utils.UTranslate.LanguagesEnum;
+
+
+public class EventAddLanguageToFile extends Event {
+
+    public static final EventType<EventAddLanguageToFile> EVENT_ADD_LANGUAGE_TO_FILE_TYPE = new EventType<>(Event.ANY, "EVENT_ADD_LANGUAGE_TO_FILE");
+
+    private final LanguagesEnum languageAdded;
+
+    // Constructors
+
+    public EventAddLanguageToFile(LanguagesEnum languageAdded) {
+        super(EVENT_ADD_LANGUAGE_TO_FILE_TYPE);
+        this.languageAdded = languageAdded;
+    }
+
+    public EventAddLanguageToFile(String languageName) {
+        super(EVENT_ADD_LANGUAGE_TO_FILE_TYPE);
+        LanguagesEnum value = LanguagesEnum.fromName(languageName);
+        if (value == null) {
+            throw new IllegalArgumentException("Language not found: " + languageName);
+        }
+        this.languageAdded = value;
+
+    }
+
+    // Getters
+
+    public LanguagesEnum getLanguageAdded() {
+        return languageAdded;
+    }
+
+}
